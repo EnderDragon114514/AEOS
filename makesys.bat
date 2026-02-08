e70d0e9e4a2ffb697386e941e39a93f735fe1ef4
LithiumProjectOffical <128273945+EnderDragon114514@users.noreply.github.com>
2026-02-08 14:17:25 +0800

    Add files via upload

--- makesys.bat
+++ makesys.bat
@@ -1,24 +1,28 @@
-rem AEOS v5.11.2 build 6937 Automatic Build Script
+rem AEOS v5.11.2 build 7049 patch 3 Automatic Build Script
 rem This script can only run in DOS mode
 @echo off
 cd .\bin
 .\bin\tcc.exe /l.\lib /i.\include /e.\src\aeos41.exe .\src\aeos41.cpp .\lib\graphics.lib
 copy .\output\aeos41.exe .\output\aeos41.old
-del .\soutput\aeos41.exe
+del .\output\aeos41.exe
 copy .\bin\aeos41.exe .\output\aeos41.exe
 .\bin\tcc.exe /l.\lib /i.\include /e.\src\bgidm.exe .\src\bgidm.cpp .\lib\graphics.lib
-copy .\src\bgidm.exe .\src\bgidm.old
-del .\src\bgidm.exe
+copy .\output\bgidm.exe .\output\bgidm.old
+del .\output\bgidm.exe
 copy .\bin\bgidm.exe .\src\bgidm.exe
 .\bin\tcc.exe /l.\lib /i.\include /e.\src\vga.exe .\src\vga.cpp .\lib\graphics.lib
-copy .\src\vga.exe .\src\vga.old
-del .\src\vga.exe
+copy .\output\vga.exe .\output\vga.old
+del .\output\vga.exe
 copy .\bin\vga.exe .\src\vga.exe
 .\bin\tcc.exe /l.\lib /i.\include /e.\src\init.exe .\src\init.cpp
-copy .\src\init.exe .\src\init.old
-del .\src\init.exe
-copy .\bin\init.exe .\src\init.exe
-.\bin\tcc.exe /l.\lib /i.\include /e.\src\51kern.exe .\src\51kern.cpp
-copy .\src\51kern.exe .\src\51kern.old
-del .\src\51kern.exe
-copy .\bin\51kern.exe .\src\51kern.exe
+copy .\output\init.exe .\output\init.old
+del .\output\init.exe
+copy .\bin\init.exe .\src\init.exe.\BIN\TCC.EXE /mh /L.\LIB /I.\INCLUDE /E.\src\51KERN.EXE .\src\51KERN.CPP .\src\MEMDEF.CPP .\src\MEMDEF.H
+pause
+copy .\output\51KERN.exe .\output\51KERN.old
+del .\output\51KERN.exe
+copy .\BIN\51KERN.EXE .\output\51KERN.exe
+.\BIN\TCC.EXE /mh /L.\LIB /I.\INCLUDE /E.\src\ABKI.EXE .\src\ABKI.CPP
+pause
+copy .\output\ABKI.exe .\output\ABKI.old
+del .\output\ABKI.exe
